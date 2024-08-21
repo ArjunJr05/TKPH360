685d529adc5d00194246fd24fe5f23e11d7e60c3
Sarathy <144701759+Sarathy2908@users.noreply.github.com>
2024-08-21 21:07:45 +0530

    Update forgot_pass.dart

--- lib/forgot_pass.dart
+++ lib/forgot_pass.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, use_build_context_synchronously, unnecessary_new
 
-import 'package:Journix/auth_pages/sign_auth.dart';
+import 'package:TKPH360/sign_auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
